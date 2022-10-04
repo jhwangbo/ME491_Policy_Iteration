c67161f092abe9adb0f328d9c416aa2ac661b419
jemin <jhwangbo@kaist.ac.kr>
2022-10-04 16:39:28 +0900

    first draft

---
+++ include/policy_iteration_STUDENTID.h
@@ -0,0 +1,16 @@
+//
+// Created by jemin on 10/4/22.
+//
+
+#pragma once
+
+template<int map_size>
+inline Eigen::Matrix<int, map_size, map_size> policyIteration(const std::array<Eigen::Matrix<double, map_size, map_size>, 4>& rewards,
+                                                              double discount_factor) {
+  Eigen::Matrix<int, map_size, map_size> optimal_policy;
+
+  //// fill in your code
+
+
+  return optimal_policy;
+}
---
+++ src/policy_iteration_runner.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include "policy_iteration_STUDENTID.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include <random>
+
+int main(int argc, char *argv[]) {
+  const int map_size = 10;
+
+  std::array<Eigen::Matrix<double, map_size, map_size>, 4> rewards; // up, right, down, left
+  const double discount_factor = 0.9;
+  srand((unsigned int) time(0));
+
+  /// this generates random a reward function
+  for (auto& rew: rewards)
+    rew = Eigen::Matrix<double, map_size, map_size>::Random().array() - 2.;
+
+  /// print out your answer
+  std::cout<<"optimal policy \n"<<policyIteration(rewards, discount_factor)<<std::endl;
+  return 0;
+}
